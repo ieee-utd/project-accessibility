e0ef89cf17ccc74057c7bc07eab7832ae72f9210
Ishpreet <ishpreet2000@gmail.com>
2020-03-11 22:38:24 -0500

    Cleaned bolierplate

---
+++ frontend/app/components/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// import win from '@nodert-win10-rs4/windows.system.profile.systemmanufacturers';
+
+export default function Home() {
+  return (
+    <div data-tid="container">
+      {/* { console.log(win.SystemSupportDeviceInfo.OperatingSystem.get()) } */}
+    </div>
+  );
+}
